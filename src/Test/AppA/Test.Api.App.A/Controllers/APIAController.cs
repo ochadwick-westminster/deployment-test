3b233f5b92c7bd0feba1af874d4334711bffa4b4
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-03-18 17:27:55 +0000

    query added (#13)
    * query added
    
    * Update APIAController.cs
    
    * Update action_validate_pr.yml
    
    * Update APIAController.cs

--- src/Test/AppA/Test.Api.App.A/Controllers/APIAController.cs
+++ src/Test/AppA/Test.Api.App.A/Controllers/APIAController.cs
@@ -8,6 +8,9 @@ public class APIAController : ControllerBase
     private readonly ILogger<APIAController> _logger;
     private readonly string password = "123";
 
+    private string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword1;";
+
+
     public APIAController(ILogger<APIAController> logger)
     {
         _logger = logger;
---
+++ src/Test/AppA/Test.Api.App.A/TestQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace Test.Api.App.A;
+
+public class TestQuery
+{
+    public TestQuery()
+    {
+        
+    }
+
+    public void Query()
+    {
+        string userInput = "'; DROP TABLE Users; --";
+        string connectionString = "your_connection_string_here";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString());
+                    }
+                }
+            }
+        }
+    }
+}
