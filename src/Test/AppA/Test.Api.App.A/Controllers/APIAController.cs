e6583b55de12df1d05d717129cd9ca749bc3f238
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-05 10:37:38 +0100

    feat: Updated A API controller

--- src/Test/AppA/Test.Api.App.A/Controllers/APIAController.cs
+++ src/Test/AppA/Test.Api.App.A/Controllers/APIAController.cs
@@ -8,7 +8,7 @@ public class APIAController : ControllerBase
     private readonly ILogger<APIAController> _logger;
     private readonly string password = "1234";
 
-    private string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword123;";
+    private string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword123e;";
 
 
     public APIAController(ILogger<APIAController> logger)
