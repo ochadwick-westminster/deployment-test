aafe579cfc0fddd5559647c2aea8f3e34bf3e79f
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-05 10:38:53 +0100

    feat: Controller B Update

--- src/Test/AppB/Test.Api.App.B/Controllers/WeatherForecastController.cs
+++ src/Test/AppB/Test.Api.App.B/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
