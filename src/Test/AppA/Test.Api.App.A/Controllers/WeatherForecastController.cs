57b60f162833140e48836bded30db5cf7e1fbd5f
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-14 04:50:22 +0100

    Fix to WeatherForecastController.cs
    Fixed an invalid value in controller

--- src/Test/AppA/Test.Api.App.A/Controllers/WeatherForecastController.cs
+++ src/Test/AppA/Test.Api.App.A/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Fish"
+        "Freezing", "Fish", "Custard"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
