8dd87f723102d1fb41d5ecf2c4ddd823219a2bff
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-15 15:44:20 +0100

    Create filter-commits-plugin.js

---
+++ .github/plugins/filter-commits-plugin.js
@@ -0,0 +1,14 @@
+// filter-commits-plugin.js
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  verifyConditions: async (pluginConfig, context) => {
+    // Assume commits are stored in a newline-separated file
+    const commitPath = path.resolve(process.cwd(), 'filtered-commits.txt');
+    const commitData = fs.readFileSync(commitPath, 'utf8');
+    const allowedCommits = new Set(commitData.trim().split('\n'));
+
+    context.commits = context.commits.filter(commit => allowedCommits.has(commit.hash));
+  }
+};
