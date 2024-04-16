a8abed31ccc449cd1451ac805275732c5b7e5292
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-16 14:23:20 +0100

    Update assess-changes-app.sh

--- .github/scripts/assess-changes-app.sh
+++ .github/scripts/assess-changes-app.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Retrieve the application name from the environment variable
-echo "env app name: $APP_NAME"
 app_name="$APP_NAME"
-echo "local app name: $app_name"
 
 # Fetch tags to ensure we have the latest tag information
 git fetch --tags
@@ -16,16 +14,11 @@ if [ -z "$LAST_TAG" ]; then
   LAST_TAG=$(git rev-list --max-parents=0 HEAD)
   echo "No version tag found, using initial commit as LAST_TAG."
 fi
-
 echo "LAST_TAG=$LAST_TAG" >> $GITHUB_ENV
-echo "Last tag: $LAST_TAG"
 
 # Determine changed files since the last tag in application and core directories
 CHANGED_FILES_APP=$(git diff --name-only $LAST_TAG HEAD | grep -E "$APP_PATH" || true)
 CHANGED_FILES_CORE=$(git diff --name-only $LAST_TAG HEAD | grep -E "$CORE_PATH" || true)
-
-echo "Changed Files in $APP_NAME: $CHANGED_FILES_APP"
-echo "Changed Files in Core: $CHANGED_FILES_CORE"
 
 # Set environment variable to indicate if the app or core has changes
 if [[ -z "$CHANGED_FILES_APP" && -z "$CHANGED_FILES_CORE" ]]; then
