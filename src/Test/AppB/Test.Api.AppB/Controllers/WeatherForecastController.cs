78b4215cf5b8d40a5f6a432283a81d5b8090aa71
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-17 16:26:55 +0100

    feat: app b change test

--- src/Test/AppB/Test.Api.AppB/Controllers/WeatherForecastController.cs
+++ src/Test/AppB/Test.Api.AppB/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "fffff"
+        "Freezing", "ffddfff"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
