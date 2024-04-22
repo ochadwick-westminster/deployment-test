0280b6b53c679e280c91aaa7c03f1595ecd288ea
ochadwick-westminster <145350800+ochadwick-westminster@users.noreply.github.com>
2024-04-22 15:23:31 +0100

    Update WeatherForecastController.cs

--- src/Test/AppB/Test.Api.AppB/Controllers/WeatherForecastController.cs
+++ src/Test/AppB/Test.Api.AppB/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "ffddfff"
+        "Freezing", "ffddefff"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
